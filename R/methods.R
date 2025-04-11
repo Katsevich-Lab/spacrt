41cc92747432419497f4bb6c0a79bfc31aa300cc
Jyotishka Ray Choudhury <jyotishkaraychoudhury@gmail.com>
2025-04-11 14:56:50 -0400

    added all necesary parameters to all the functions

--- R/methods.R
+++ R/methods.R
@@ -10,7 +10,8 @@
 #' (values can be \code{gaussian}, \code{binomial}, \code{poisson}, \code{negative.binomial}, etc).
 #' @param Y_on_Z_fam The GLM family for the regression of Y on Z
 #' (values can be \code{gaussian}, \code{binomial}, \code{poisson}, \code{negative.binomial}, etc).
-#' @param fitting_method The fitting method for the regressions X on Z and Y on Z.
+#' @param fitting_X_on_Z The fitting method for the regression X on Z.
+#' @param fitting_Y_on_Z The fitting method for the regression Y on Z.
 #'
 #' @return A named list with fields \code{test_stat} and \code{left_side_p_value},
 #' \code{right_side_p_value}, \code{both_side_p_value}.
@@ -23,19 +24,23 @@
 #' X_on_Z_fam <- "binomial"
 #' Y_on_Z_fam <- "poisson"
 #' results <- GCM(data, X_on_Z_fam, Y_on_Z_fam,
-#'                fitting_method = 'glm')
+#'                fitting_X_on_Z = 'rf',
+#'                fitting_Y_on_Z = 'glm')
 #'
 #' @export
 GCM <- function(data, X_on_Z_fam, Y_on_Z_fam,
-                fitting_method = 'glm') {
+                fitting_X_on_Z = 'glm',
+                fitting_Y_on_Z = 'glm') {
 
   # extract (X,Y,Z) from inputted data
   X <- data$X; Y <- data$Y; Z <- data$Z
   n <- length(X)
 
   fitted_vals <- fit_models(data = data,
-                            fitting_method = fitting_method,
-                            X_on_Z_fam = X_on_Z_fam, Y_on_Z_fam = Y_on_Z_fam)
+                            X_on_Z_fam = X_on_Z_fam,
+                            Y_on_Z_fam = Y_on_Z_fam,
+                            fitting_X_on_Z = fitting_X_on_Z,
+                            fitting_Y_on_Z = fitting_Y_on_Z)
 
   X_on_Z_fit_vals <- fitted_vals$X_on_Z_fit_vals
   Y_on_Z_fit_vals <- fitted_vals$Y_on_Z_fit_vals
@@ -68,41 +73,40 @@ GCM <- function(data, X_on_Z_fam, Y_on_Z_fam,
 #' (values can be \code{gaussian}, \code{binomial}, \code{poisson}, etc).
 #' @param Y_on_Z_fam The GLM family for the regression of Y on Z
 #' (values can be \code{gaussian}, \code{binomial}, \code{poisson}, etc).
-#' @param fitting_method The fitting method for the regressions X on Z and Y on Z.
+#' @param fitting_X_on_Z The fitting method for the regression X on Z.
+#' @param fitting_Y_on_Z The fitting method for the regression Y on Z.
 #' @param B The number of resamples to draw.
-#' @param normalize A logical variable indicating whether the dCRT should be
-#' normalized.
-#' @param return_resamples A logical variable indicating whether to return the
-#' resampled test statistics.
 #'
 #' @return A named list with fields \code{test_stat}, \code{left_side_p_value},
-#' \code{right_side_p_value}, \code{both_side_p_value} and
-#' \code{resamples}. Here, \code{resamples} is a vector of length \code{B}. It
-#' is returned only if \code{return_resamples == TRUE}.
+#' \code{right_side_p_value}, and \code{both_side_p_value}.
 #'
 #' @examples
-#' n <- 80; p <- 2; B <- 100; normalize <- FALSE; return_resamples <- FALSE
+#' n <- 80; p <- 2; B <- 100
 #' data <- list(X = rbinom(n = n, size = 1, prob = 0.2),
 #'              Y = rpois(n = n, lambda = 1),
 #'              Z = matrix(rnorm(n = n*p, mean = 0, sd = 1), nrow = n, ncol = p))
 #' X_on_Z_fam <- "binomial"
 #' Y_on_Z_fam <- "poisson"
 #' results <- dCRT(data, X_on_Z_fam, Y_on_Z_fam,
-#'                 fitting_method = 'glm', B = 2000,
-#'                 normalize, return_resamples)
+#'                 fitting_X_on_Z = 'rf',
+#'                 fitting_Y_on_Z = 'glm',
+#'                 B = 2000)
 #'
 #' @export
 dCRT <- function(data, X_on_Z_fam, Y_on_Z_fam,
-                 fitting_method = 'glm',
-                 B = 2000, normalize = FALSE, return_resamples = FALSE) {
+                 fitting_X_on_Z = 'glm',
+                 fitting_Y_on_Z = 'glm',
+                 B = 2000) {
 
   # extract (X,Y,Z) from inputted data
   X <- data$X; Y <- data$Y; Z <- data$Z
   n <- length(X)
 
   fitted_vals <- fit_models(data = data,
-                            fitting_method = fitting_method,
-                            X_on_Z_fam = X_on_Z_fam, Y_on_Z_fam = Y_on_Z_fam)
+                            X_on_Z_fam = X_on_Z_fam,
+                            Y_on_Z_fam = Y_on_Z_fam,
+                            fitting_X_on_Z = fitting_X_on_Z,
+                            fitting_Y_on_Z = fitting_Y_on_Z)
 
   X_on_Z_fit_vals <- fitted_vals$X_on_Z_fit_vals
   Y_on_Z_fit_vals <- fitted_vals$Y_on_Z_fit_vals
@@ -150,47 +154,37 @@ dCRT <- function(data, X_on_Z_fam, Y_on_Z_fam,
 #' (values can be \code{gaussian}, \code{binomial}, \code{poisson}, etc).
 #' @param Y_on_Z_fam The GLM family for the regression of Y on Z
 #' (values can be \code{gaussian}, \code{binomial}, \code{poisson}, etc).
-#' @param fitting_method The fitting method for the regressions X on Z and Y on Z.
-#' @param normalize A logical variable indicating whether the spaCRT is based on
-#' the normalized test statistic.
-#' @param return_cdf A logical variable indicating whether to return the CDF
+#' @param fitting_X_on_Z The fitting method for the regression X on Z.
+#' @param fitting_Y_on_Z The fitting method for the regression Y on Z.
 #' @param R Upper bound of search space for the saddlepoint
 #'
 #' @return A named list with fields \code{test_stat}, \code{left_side_p_value},
-#' \code{right_side_p_value}, \code{both_side_p_value}, \code{cdf}, and
+#' \code{right_side_p_value}, \code{both_side_p_value}, and
 #' \code{gcm.default}.
-#' Here, cdf is a function that takes in a value t and returns the
-#' saddlepoint approximation to the CDF of the resampling distribution of the
-#' test statistic evaluated at t. This function is returned only if
-#' return_cdf == TRUE.
 #' gcm.default returns TRUE if GCM was employed due to the failure of spaCRT.
 #'
 #' @examples
-#' n <- 50; p <- 6; normalize <- FALSE; return_cdf <- FALSE
-#' data <- list(X = rbinom(n = n, size = 1, prob = 0.2),
-#'              Y = rpois(n = n, lambda = 1),
-#'              Z = matrix(rnorm(n = n*p, mean = 0, sd = 1), nrow = n, ncol = p))
-#' X_on_Z_fam <- "binomial"
-#' Y_on_Z_fam <- "poisson"
-#' spaCRT(data, X_on_Z_fam, Y_on_Z_fam, fitting_method = 'glm', normalize)
-#'
-#' n <- 50; p <- 4; normalize <- FALSE; return_cdf <- FALSE
+#' n <- 50; p <- 4
 #' data <- list(X = rbinom(n = n, size = 1, prob = 0.2),
 #'              Y = rbinom(n = n, size = 1, prob = 0.7),
 #'              Z = matrix(rnorm(n = n*p, mean = 0, sd = 1), nrow = n, ncol = p))
 #' X_on_Z_fam <- "binomial"
 #' Y_on_Z_fam <- "binomial"
-#' spaCRT(data, X_on_Z_fam, Y_on_Z_fam, fitting_method = "rf")
+#' spaCRT(data, X_on_Z_fam, Y_on_Z_fam,
+#'        fitting_X_on_Z = 'rf',
+#'        fitting_Y_on_Z = 'glm')
 #'
 #' @export
 spaCRT <- function(data, X_on_Z_fam, Y_on_Z_fam,
-                   fitting_method = 'glm',
-                   normalize = FALSE, return_cdf = FALSE,
+                   fitting_X_on_Z = 'glm',
+                   fitting_Y_on_Z = 'glm',
                    R = 5) {
 
   fitted_vals <- fit_models(data = data,
-                            X_on_Z_fam = X_on_Z_fam, Y_on_Z_fam = Y_on_Z_fam,
-                            fitting_method = fitting_method) |> suppressWarnings()
+                            X_on_Z_fam = X_on_Z_fam,
+                            Y_on_Z_fam = Y_on_Z_fam,
+                            fitting_X_on_Z = fitting_X_on_Z,
+                            fitting_Y_on_Z = fitting_Y_on_Z) |> suppressWarnings()
 
   spa_result <- spa_cdf(X = data$X, Y = data$Y,
                         X_on_Z_fit_vals = fitted_vals$X_on_Z_fit_vals,
@@ -206,88 +200,6 @@ spaCRT <- function(data, X_on_Z_fam, Y_on_Z_fam,
 
 
 
-
-#####################################################################################
-#' Score Test
-#'
-#' \code{score.test} is a function carrying out the saddlepoint approximation to the
-#' dCRT based on GLMs for `X|Z` and `Y|Z`.
-#'
-#' @param data A named list with fields \code{X} (an nx1 vector for the predictor
-#' variable of interest), \code{Y} (an nx1 response vector), and \code{Z}
-#' (an nxp matrix of covariates).
-#' @param X_on_Z_fam The GLM family for the regression of X on Z
-#' (values can be \code{gaussian}, \code{binomial}, \code{poisson}, etc).
-#' @param Y_on_Z_fam The GLM family for the regression of Y on Z
-#' (values can be \code{gaussian}, \code{binomial}, \code{poisson}, etc).
-#'
-#' @examples
-#' n <- 50; p <- 2; normalize <- FALSE; return_cdf <- FALSE
-#' data <- list(X = rbinom(n = n, size = 1, prob = 0.2),
-#'              Y = rpois(n = n, lambda = 1),
-#'              Z = matrix(rnorm(n = n*p, mean = 0, sd = 1), nrow = n, ncol = p))
-#' X_on_Z_fam <- "binomial"
-#' Y_on_Z_fam <- "negative.binomial"
-#' results <- score.test(data, X_on_Z_fam, Y_on_Z_fam)
-#' results$test_stat
-#'
-#' @return A named list with fields \code{test_stat} and \code{left_side_p_value},
-#' \code{right_side_p_value} and \code{both_side_p_value}.
-#'
-#' @export
-score.test <- function(data, X_on_Z_fam, Y_on_Z_fam){
-
-  # extract (X,Y,Z) from inputted data
-  X <- data$X; Y <- data$Y; Z <- data$Z
-  n <- length(X)
-
-  # fit X on Z regression
-  X_on_Z_fit <- suppressWarnings(stats::glm(X ~ Z, family = X_on_Z_fam))
-
-  # fit Y on Z regression
-  if(Y_on_Z_fam == "negative.binomial"){
-    # First try to fit the model using glm.nb
-    temp.result <- tryCatch({
-      Y_on_Z_fit <- suppressWarnings(MASS::glm.nb(Y ~ Z))
-      NB.disp.param <- Y_on_Z_fit$theta
-      list(Y_on_Z_fit = Y_on_Z_fit, NB.disp.param = NB.disp.param)
-    },
-    error = function(e) {
-      aux_info_Y_on_Z <- nb_precomp(list(Y = Y, Z = Z))
-
-      Y_on_Z_fit <- suppressWarnings(stats::glm(Y ~ Z,
-                              family = MASS::negative.binomial(aux_info_Y_on_Z$theta_hat),
-                              mustart = aux_info_Y_on_Z$fitted_values))
-      NB.disp.param <- aux_info_Y_on_Z$theta_hat
-
-      list(Y_on_Z_fit = Y_on_Z_fit, NB.disp.param = NB.disp.param)
-    })
-  }else if(Y_on_Z_fam == 'poisson'){
-    aux_info_Y_on_Z <- nb_precomp(list(Y = Y, Z = Z))
-
-    Y_on_Z_fit <- suppressWarnings(stats::glm(Y ~ Z,
-                                              family = stats::poisson(),
-                                              mustart = aux_info_Y_on_Z$fitted_values))
-    NB.disp.param <- NA
-
-    temp.result <- list(Y_on_Z_fit = Y_on_Z_fit, NB.disp.param = NB.disp.param)
-  }else{
-    Y_on_Z_fit <- suppressWarnings(stats::glm(Y ~ Z, family = Y_on_Z_fam))
-    NB.disp.param <- NA
-
-    temp.result <- list(Y_on_Z_fit = Y_on_Z_fit, NB.disp.param = NB.disp.param)
-  }
-
-  # perform score test
-  test_stat <- statmod::glm.scoretest(fit = temp.result$Y_on_Z_fit, x2 = X)
-
-  # return test statistic, score test p-values, and related quantities
-  return(list(test_stat = test_stat,
-              p.left = stats::pnorm(test_stat, lower.tail = TRUE),
-              p.right = stats::pnorm(test_stat, lower.tail = FALSE),
-              p.both = 2*stats::pnorm(abs(test_stat), lower.tail = FALSE),
-              NB.disp.param = temp.result$NB.disp.param))
-}
 
 
 
