e8b7d209c2e835a66dac1e50244516115f771017
Jyotishka Ray Choudhury <jyotishkaraychoudhury@gmail.com>
2025-05-03 04:31:39 -0400

    deleted, as .onLoad no longer necessary

--- R/zzz.R
+++
@@ -1,7 +0,0 @@
-.onLoad <- function(libname, pkgname) {
-  # Dynamically retrieve the unexported function from the sceptre namespace
-  if (requireNamespace("sceptre", quietly = TRUE)) {
-    estimate_theta <- get("estimate_theta", envir = asNamespace("sceptre"))
-    assign("estimate_theta", estimate_theta, envir = parent.env(environment()))
-  }
-}
