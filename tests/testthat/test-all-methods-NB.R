af82f20f0ccb69e8c763f34a3a025616280de7b0
Jyotishka Ray Choudhury <jyotishkaraychoudhury@gmail.com>
2025-05-07 00:27:29 -0400

    wrote the arguments of the main functions in the new format

--- tests/testthat/test-all-methods-NB.R
+++ tests/testthat/test-all-methods-NB.R
@@ -33,20 +33,17 @@ fitting_X_on_Z <- 'rf'; fitting_Y_on_Z <- 'glm'
 
 
 results_GCM <- GCM(X, Y, Z,
-                   X_on_Z_fam, Y_on_Z_fam,
-                   fitting_X_on_Z = fitting_X_on_Z,
-                   fitting_Y_on_Z = fitting_Y_on_Z)
+                   family = list(XZ = X_on_Z_fam, YZ = Y_on_Z_fam),
+                   method = list(XZ = fitting_X_on_Z, YZ = fitting_Y_on_Z))
 
 results_dCRT <- dCRT(X, Y, Z,
-                     X_on_Z_fam, Y_on_Z_fam,
-                     fitting_X_on_Z = fitting_X_on_Z,
-                     fitting_Y_on_Z = fitting_Y_on_Z,
+                     family = list(XZ = X_on_Z_fam, YZ = Y_on_Z_fam),
+                     method = list(XZ = fitting_X_on_Z, YZ = fitting_Y_on_Z),
                      B = 10000)
 
 results_spaCRT <- spaCRT(X, Y, Z,
-                         X_on_Z_fam, Y_on_Z_fam,
-                         fitting_X_on_Z = fitting_X_on_Z,
-                         fitting_Y_on_Z = fitting_Y_on_Z)
+                         family = list(XZ = X_on_Z_fam, YZ = Y_on_Z_fam),
+                         method = list(XZ = fitting_X_on_Z, YZ = fitting_Y_on_Z))
 
 results_test_stat <- c(results_GCM$test_stat, results_dCRT$test_stat, results_spaCRT$test_stat)
 results_p_value <- c(results_GCM$p_value, results_dCRT$p_value, results_spaCRT$p_value)
